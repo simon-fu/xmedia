131f95b7fd4dfcffead1e1375f929d2b7dd1a781
simon-fu <simonok@139.com>
2016-07-27 12:21:08 +0800

    add dev-run.sh

---
+++ xctools/dev-run.sh
@@ -0,0 +1,7 @@
+
+# ./bin/udp_dump ./output/dump.udp 10000 10002
+
+# ./bin/udp_replay output/dump-video-rtp-only.udp 127.0.0.1
+
+# ./bin/convert_dump_to_h264 output/dump-video-rtp-only.udp ./output/output.h264 10002
+
