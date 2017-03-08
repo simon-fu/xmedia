39e83fddc32cc7335fcb0527d90710422385b855
simon-fu <simonok@139.com>
2017-03-08 12:46:56 +0800

    remove webm

--- webm/build.sh
+++
@@ -1,6 +0,0 @@
-gcc `pkg-config libavcodec libavformat libavutil --cflags` main.c rav_record.c -o demo1 `pkg-config libavcodec libavformat libavutil --libs`
-
-gcc `pkg-config libavcodec libavformat libavutil libswscale libswresample --cflags` muxingav.c -o muxingav `pkg-config libavcodec libavformat libavutil libswscale libswresample --libs`
-
-
-
