7beb006ff1f1577689341200a1012740ad1dc056
simon-fu <simonok@139.com>
2017-03-09 17:36:03 +0800

    support compiling medooze-media-server

--- record/build.sh
+++ record/build.sh
@@ -2,5 +2,14 @@ g++ `pkg-config libavcodec libavformat libavutil --cflags` main.cc rav_record.cc
 
 gcc `pkg-config libavcodec libavformat libavutil libswscale libswresample --cflags` muxingav.c -o muxingav `pkg-config libavcodec libavformat libavutil libswscale libswresample --libs`
 
-
+# g++ `pkg-config libavcodec libavformat libavutil --cflags` \
+# 	main.cc \
+# 	rav_record.cc \
+# 	medooze-media-server/mod/RTPPacket.cpp \
+# 	medooze-media-server/mod/RTPHeader.cpp \
+# 	medooze-media-server/mod/RTPHeaderExtension.cpp \
+# 	medooze-media-server/mod/vp8depacketizer.cpp \
+# 	-I./medooze-media-server/mod/ \
+# 	-o demo1 \
+# 	`pkg-config libavcodec libavformat libavutil --libs`
 
