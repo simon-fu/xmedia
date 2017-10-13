605484bcc448e45d8b52912d34fd1ea9cb4e8d5e
simon-fu <simonok@139.com>
2017-10-13 19:16:52 +0800

    bwe: remove some unused code; add TOO

--- bwe/main.cpp
+++ bwe/main.cpp
@@ -162,6 +162,7 @@ enum XOPTS {
     XOPT_MAX
 };
 
+// TODO: add arg of init_q, init_r, skip first column, etc
 static 
 const struct xcmd_option * get_app_options(){
 
@@ -335,59 +336,4 @@ int main(int argc, char ** argv){
 }
 
 
-#if 0
-int main(void)
-{
-    int i = 0;
-    kalman1_state state1;
-    float *data = NULL;
-    int data_len = 0;
-    float out1 = 0;
-    /* For 2 Dimension */
-    kalman2_state state2;
-    float init_x[2];
-    float init_p[2][2] = {{10e-6,0}, {0,10e-6}};
-    float out2 = 0;
-
-    data = x1;
-    data_len = sizeof(x1)/sizeof(float);
-    // 1 dimension
-    kalman1_init(&state1, data[1], 5e2);
-    // 2 dimension
-    init_x[0] = data[0];
-    init_x[1] = data[1] - data[0];
-    kalman2_init(&state2, init_x, init_p);
-    printf("%d %d %d\n", data, data_len-2, data_len-2);
-    for (i=2; i<data_len; i++) {  // Filter start from 2
-        printf("%.2f", data[i]);      // Original data
-        /* 1 dimension */
-        out1 = kalman1_filter(&state1, data[i]);
-        printf(" %.2f", out1);  // Filter result
-        /* 2 dimension */
-        out2 = kalman2_filter(&state2, data[i]);
-        printf(" %.2f\n", out2);  // Filter result
-    }
-
-    data = x2;
-    data_len = sizeof(x2)/sizeof(float);
-    // 1 dimension
-    kalman1_init(&state1, data[1], 5e2);
-    // 2 dimension
-    init_x[0] = data[0];
-    init_x[1] = data[1] - data[0];
-    kalman2_init(&state2, init_x, init_p);
-    printf("%d %d %d\n", data, data_len-2, data_len-2);
-    for (i=2; i<data_len; i++) {  // Filter start from 2
-        printf("%.2f", data[i]);      // Original data
-        /* 1 dimension */
-        out1 = kalman1_filter(&state1, data[i]);
-        printf(" %.2f", out1);  // Filter result
-        /* 2 dimension */
-        out2 = kalman2_filter(&state2, data[i]);
-        printf(" %.2f\n", out2);  // Filter result
-    }
-
-    return 0;
-}
-#endif
 
