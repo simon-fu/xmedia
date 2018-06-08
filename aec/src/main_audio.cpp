6eedf1c2ffd2c139ebda4c4726029bda555d7ccd
simon-fu <simonok@139.com>
2018-06-08 18:26:14 +0800

    aec: add PESQ

---
+++ aec/src/main_audio.cpp
@@ -0,0 +1,9 @@
+//
+//  main_audio.cpp
+//  app_audio
+//
+//  Created by simon on 2018/6/8.
+//  Copyright © 2018年 easemob. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ aec/xcode/app_audio/app_audio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  app_audio
+//
+//  Created by simon on 2018/6/8.
+//  Copyright © 2018年 easemob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
