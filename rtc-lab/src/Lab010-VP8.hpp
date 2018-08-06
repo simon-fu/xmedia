7a22e82fad3cc6f4707b6473f507ec845aa85642
simon-fu <simonok@139.com>
2018-08-06 14:27:04 +0800

    rtc-lab: change lab-vp8 to Lab010-VP8

---
+++ rtc-lab/src/Lab010-VP8.hpp
@@ -0,0 +1,12 @@
+
+
+#ifndef Lab010_VP8_hpp
+#define Lab010_VP8_hpp
+
+#include <stdio.h>
+
+extern "C"{
+    int lab_vp8_main(int argc, char* argv[]);
+}
+
+#endif /* Lab010_VP8_hpp */
--- rtc-lab/xcode/RTCLab/main.cpp
+++ rtc-lab/xcode/RTCLab/main.cpp
@@ -1,16 +1,9 @@
-//
-//  main.cpp
-//  RTCLab
-//
-//  Created by simon on 2018/7/24.
-//  Copyright © 2018年 easemob. All rights reserved.
-//
+
 
 #include <iostream>
+#include "Lab010-VP8.hpp"
 #include "Lab020-Beauty.hpp"
-extern "C"{
-    int lab_vp8_main(int argc, char* argv[]);
-}
+
 int main(int argc, char * argv[]) {
     int ret = 0;
     //ret = lab_vp8_main(argc, argv);
